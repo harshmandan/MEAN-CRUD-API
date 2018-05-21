0cf9625a6bf7c72f72be64b6da3cdbd1c6a2981b
Harsh Mandan <harshsinghmandan@gmail.com>
2018-05-21 23:28:04 +0530

    Initial commit!

---
+++ routes/Products.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var Product = require('../models/Product.js');
+
+//get all
+router.get('/', function(req, res, next) {
+  Product.find(function (err, products) {
+    if (err) return next(err);
+    res.json(products);
+  });
+});
+
+//get by id
+router.get('/:id', function(req, res, next) {
+  Product.findById(req.params.id, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+//save
+router.post('/', function(req, res, next) {
+  Product.create(req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+//update
+router.put('/:id', function(req, res, next) {
+  Product.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+//delete
+router.delete('/:id', function(req, res, next) {
+  Product.findByIdAndRemove(req.params.id, req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+module.exports = router;
